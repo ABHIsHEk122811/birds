5ea7fd2d015f346f24a9dfe3f4456c9234b17c7c
Pranav Mittal <33569604+SSHANU17@users.noreply.github.com>
2019-01-15 00:30:27 +0530

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+
 void main()
 {
 runApp(new MaterialApp(
@@ -9,15 +12,36 @@ runApp(new MaterialApp(
 
 ));
 }
+
 class MyApp extends StatefulWidget{
 @override
+
    _State createState()=> new _State();
 
 }
 class _State extends State<MyApp>{
   @override
    Widget build (BuildContext context){
-     return new Scaffold(
+    Widget image_carousel=new Container(
+      height: 150,//width: 50,
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('products/a1.jpg'),
+          AssetImage('products/a2.jpg'),
+          //AssetImage('products/a3.jpg'),
+          //AssetImage('products/a4.jpg'),
+
+        ],
+        autoplay: true,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 200),
+        dotSize: 4.0,dotColor: Colors.amber,
+        dotSpacing: 10.0,noRadiusForIndicator: true,overlayShadow: false,
+        indicatorBgPadding: 8.0,dotBgColor: Colors.white24,
+      ),
+    );
+    return new Scaffold(
         appBar : new AppBar(
           elevation: 0.5,
           backgroundColor: Colors.amber,
@@ -79,7 +103,7 @@ class _State extends State<MyApp>{
              InkWell(
                onTap: (){},
                child: ListTile(
-                 title: Text('Favourite'),
+                 title: Text('My Wishlist'),
                  leading: Icon(Icons.favorite,color: Colors.red,),
                ),
              ),
@@ -94,6 +118,13 @@ class _State extends State<MyApp>{
              InkWell(
                onTap: (){},
                child: ListTile(
+                 title: Text('Register as a Seller'),
+                 leading: Icon(Icons.person, color: Colors.grey,),
+               ),
+             ),
+             InkWell(
+               onTap: (){},
+               child: ListTile(
                  title: Text('About'),
                  leading: Icon(Icons.help, color: Colors.green,),
                ),
@@ -101,16 +132,25 @@ class _State extends State<MyApp>{
            ],
          ),
        ),
-       body: new Container(
+
+      body: new ListView(
+        children: <Widget>[
+          image_carousel,
+          new Padding(padding: const EdgeInsets.all(8.0)),
+          new Text('   Categories')
+        ],
+      )
+
+      /*Container(
            padding: new EdgeInsets.all(32.0),
            child: new Center(
               child: new Column(
                  children: <Widget>[
-                    new Text('HELLO WORLD')
+                    new Text('LOOK FORWARD')
                  ],
               ),
            ),
-        ),
+        ),*/
      );
   }
 }
